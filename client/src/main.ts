500fafd77578567f5805020323d8b426c4789594
Alex Paquibot <115098095+alexpqbt@users.noreply.github.com>
2026-02-17 15:28:04 +0800

    feat: Add UI, no functionality yet

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,1 +1,32 @@
-console.log("Hello Typescript!")
+const videoElem = document.getElementById("video") as HTMLMediaElement
+const startBtn = document.getElementById("start")!
+const stopBtn = document.getElementById("stop")!
+
+const displayMediaOptions = {
+  video: true,
+  audio: false
+}
+
+async function startCapture() {
+  try {
+    videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+function stopCapture() {
+  let stream = videoElem.srcObject as MediaStream
+  let tracks = stream.getTracks()
+
+  tracks.forEach((track) => track.stop())
+  videoElem.srcObject = null
+}
+
+startBtn.addEventListener("click", (e) => {
+  startCapture()
+})
+
+stopBtn.addEventListener("click", (e) => {
+  stopCapture()
+})
